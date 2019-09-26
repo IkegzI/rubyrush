51a52612b33e8c0c79ae6f2cef68a1992e692285
IkegzI <kegz@mail.ru>
2019-09-26 23:00:41 +0800

    cart game

---
+++ game/project2.rb
@@ -0,0 +1,7 @@
+cart = ['6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A', 'Joker']
+
+suit = ["\xE2\x99\xA3", "\xE2\x99\xA5", "\xE2\x99\xA6", "\xE2\x99\xA0"]
+puts " ___"
+puts "| #{suit.sample} |"
+puts "| #{cart.sample} |"
+puts "====="
