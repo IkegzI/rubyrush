e0c9c2d820ea615924283d40d983c0f9040a2cfb
IkegzI <kegz@oit-02-nettop.localdomain>
2019-09-27 11:09:23 +0800

    undefined?

---
+++ variable/project1.rb
@@ -0,0 +1,17 @@
+puts 
+a = 'cdd'
+
+
+if defined?(a)
+puts "Переменная 'a' определена"
+else
+puts "Переменная 'b' определена" 
+end
+if defined?(b)
+ puts "Переменная 'a' не определена"
+else
+ puts "Переменная 'b' не определена" 
+end
+puts 
+
+
