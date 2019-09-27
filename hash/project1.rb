a4c903cad960910a173fe4b56caa65a59c94ac47
IkegzI <kegz@mail.ru>
2019-09-27 20:35:43 +0800

    hash in hash

--- hash/project1.rb
+++ hash/project1.rb
@@ -1,11 +1,30 @@
-ticket = {	name: 'Иванов А.В.',
-			pass: '12 34 432123',
-			pt: 'Москва - Владивосток',
-			ticket: '43252312515235'
+tickets = {
+    place_1: {
+        name: 'Иванов А.В.',
+        pass: '12 34 432123',
+        pt: 'Москва - Владивосток',
+        ticket: '43252365656565235'
+    },
+    place_2: {
+        name: 'Иванов А.В.',
+        pass: '12 34 432123',
+        pt: 'Москва - Владивосток',
+        ticket: '4325231756756752515235'
+    },
+    place_3: {
+        name: 'Иванов А.В.',
+        pass: '12 34 432123',
+        pt: 'Москва - Владивосток',
+        ticket: '43252312566666635'
+    }
 }
 
-
-puts "Маршрут: #{ticket[:pt]}"
-puts "Билет № #{ticket[:ticket]}"
-puts "ФИО пассажира: #{ticket[:name]}"
-puts "Паспорт : #{ticket[:pass]}"
+tickets.each do |key,item|
+  puts
+  puts "*  *  *  #{key}  *  *  *"
+  puts "Маршрут: #{item[:pt]}"
+  puts "Билет № #{item[:ticket]}"
+  puts "ФИО пассажира: #{item[:name]}"
+  puts "Паспорт : #{item[:pass]}"
+end
+puts
