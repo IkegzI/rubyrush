53d22d857bfe015e7ad9dc8cdca0aeebed6b7f94
IkegzI <kegz12@mail.ru>
2019-10-15 23:35:10 +0800

    xml-parse visit cart v 1.1

--- XML/project1.rb
+++ XML/project1.rb
@@ -11,26 +11,26 @@ def file_data(file)
       unless (elem.attribute('number').nil? and name_section != 'profession')
         number = elem.attribute('number').value
       else
-       number = nil
+        number = nil
       end
 
       if number.nil?
-       section[name_section] = {}
-       else
+        section[name_section] = elem.text || {}
+      else
         section[name_section] = {} if section[name_section].nil?
         section[name_section][number] = {}
       end
 
       elem.attributes.each_key do |attrs|
-       if number.nil?
+        if number.nil?
           section[name_section][elem.attribute("#{attrs}").name] = elem.attribute("#{attrs}").value
-          elsif attrs != 'number'
+        elsif attrs != 'number'
           section[name_section][number][elem.attribute("#{attrs}").name] = elem.attribute("#{attrs}").value
         end
       end
 
     end
-end
+  end
   section
 end
 
@@ -39,16 +39,26 @@ file = File.open("./project1/visit_cart.xml")
 data = file_data file
 
 data[:name].each_value { |value| print value + ' ' }
-puts
-puts "Contacts:"
-data[:contacts].each_key { |key| print key + '=>' + data[:contacts][key] + ' ' }
+unless data[:contacts].nil?
+ puts
+ puts "Contacts:"
+ data[:contacts].each_key { |key| print key + '=>' + data[:contacts][key] + ' ' }
+end
 puts
 puts "Locate:"
 data[:locate].each_value { |value| print value + ' ' }
 puts
-puts 'Profession:'
-data[:profession].each_key do |key|
-  data[:profession][key].each_value{|k| print "#{k} "  }
-  puts
+unless data[:profession].nil?
+  puts 'Profession:'
+  if data[:profession].size > 1
+    data[:profession].each_key do |key|
+      data[:profession][key].each_value { |k| print "#{k} " }
+      puts
+    end
+  else
+   data[:profession].each_value { |k| print "#{k} " }
+  end
 end
+puts 'Addition: '
+puts data[:letter]
 
